c3fa817c94eb4c37c33e017d173a4a2f026f4016
Maria Karpukhina <myshka_n@mail.ru>
2020-02-25 19:46:44 +0300

    Initial Commit

---
+++ HomeWork 2.1.2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork 2.1.2
+//
+//  Created by Mary Jane on 25.02.2020.
+//  Copyright © 2020 Maria Karpukhina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
